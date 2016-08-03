6a764192584425deb2d93f8307d2aaf25b4ccad7
Ryan Pow <powryan@ymail.com>
2016-08-03 10:42:52 +0800

    Signed-off-by: Ryan Pow <powryan@ymail.com>

--- connectdb.php
+++ connectdb.php
@@ -3,7 +3,7 @@ function Connection(){
 	$servername = "127.11.44.2:3306";
 	$username = "adminAXejd2g";
 	$password = "QGBDCjp_R7s7";
-	$dbname = "mySQL";
+	$dbname = "mysql";
 	$index = 0;
    	
 	$conn = new mysqli($servername, $username, $password, $dbname);
