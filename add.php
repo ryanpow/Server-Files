9df080ae5d5e93046929659564ba27c078b6f4f1
Ryan Pow <powryan@ymail.com>
2016-08-03 10:42:52 +0800

    Signed-off-by: Ryan Pow <powryan@ymail.com>

---
+++ add.php
@@ -0,0 +1,30 @@
+<?php
+   	include("connectdb.php");
+   	
+   	$link=Connection();
+
+	$UserID=$_POST["Userid"];
+	$Location=$_POST["Location"];
+	$Wifi=$_POST["Wifi"];
+
+	if($Location==null){	
+	$query = "UPDATE GPSAccount set Wifi='".$Wifi."' where UserID='".$UserID."'";
+   	}
+	else if($Wifi==null){
+	$query = "UPDATE GPSAccount set Location='".$Location."' where UserID='".$UserID."'";
+	} 
+	
+	else{
+	 $query = "UPDATE GPSAccount set Wifi='".$Wifi."',Location='".$Location."' where UserID='".$UserID."'";
+	 }
+   	if ($link->query($query) === TRUE) {
+   	    echo "New record created successfully <br>";
+   	} else {
+   	    echo "Error: " . $sql . "<br>" . $conn->error;
+   	}
+   	
+   	$link->close();
+   	header("Location: readDatabase.php");
+?>
+
+		
