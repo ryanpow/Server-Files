b7accc5aaaf55132c9231b366742b15f206ab606
Ryan Pow <powryan@ymail.com>
2016-08-15 16:19:58 +0800

    Signed-off-by: Ryan Pow <powryan@ymail.com>

--- Register.php
+++ Register.php
@@ -3,13 +3,13 @@
    	
    	$link=Connection();
 
-	$UserID=$_POST["UserID"];
-	$Username=$_POST["Username"];
-	$Password=$_POST["Password"];
-	$Location=$_POST["Location"];
-	$Wifi=$_POST["Wifi"];
+	$UserIDAdd=$_POST["UserID"];
+	$UsernameAdd=$_POST["Username"];
+	$PasswordAdd=$_POST["Password"];
+	$LocationAdd=$_POST["Location"];
+	$WifiAdd=$_POST["Wifi"];
 	
-	$query = "insert into GPSAccount values('".UserID."','".Username."','".Password."','".Location."','".Wifi."')";
+	$query = "insert into GPSAccount values('".UserIDAdd."','".UsernameAdd."','".PasswordAdd."','".LocationAdd."','".WifiAdd."')";
    	
    	if ($link->query($query) === TRUE) {
    	    echo "New record created successfully <br>";
